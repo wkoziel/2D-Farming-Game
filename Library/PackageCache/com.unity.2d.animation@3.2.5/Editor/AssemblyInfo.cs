c65f1a7749bca74f99739b741ca3306e58e688e7
Wojciech Kozieł <44378819+fern00@users.noreply.github.com>
2020-11-15 22:38:47 +0100

    Library remove

--- Library/PackageCache/com.unity.2d.animation@3.2.5/Editor/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("Unity.2D.Animation.Tests.EditorTests")]
-[assembly: InternalsVisibleTo("Unity.2D.PsdImporter.Editor")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
--- Library/PackageCache/com.unity.2d.spriteshape@3.0.14/Documentation~/TableOfContents.md
+++
@@ -1,5 +0,0 @@
-* [Overview](index.md)
-* [Sprite Shape Profile](SSProfile.md)
-* [Sprite Shape Controller](SSController.md)
-* [Enabling Collision](SSCollision.md)
-
