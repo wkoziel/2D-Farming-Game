f235404b8adaeff080e29909dfe9497444eda332
PiotrHadam <drpiotr1997@gmail.com>
2021-01-24 17:34:39 +0100

    Main Menu added

---
+++ Assets/Menu/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isStart;
+    public bool isAbout;
+    public bool isQuit;
+	public bool isBack;
+
+	void OnMouseUp()
+	{
+		if (isStart)
+		{
+			Application.LoadLevel(1);
+		}
+		if (isAbout)
+        {
+			Application.LoadLevel(2);
+        }
+		if (isQuit)
+		{
+			Application.Quit();
+		}
+		if (isBack)
+        {
+			Application.LoadLevel(0);
+        }
+	}
+}
---
+++ Assets/Menu/MouseHover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+	void Start()
+	{
+		GetComponent<Renderer>().material.color = Color.white;
+	}
+
+	void OnMouseEnter()
+	{
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+
+	void OnMouseExit()
+	{
+		GetComponent<Renderer>().material.color = Color.white;
+	}
+}
