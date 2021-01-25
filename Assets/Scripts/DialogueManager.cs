516abea3705d909051dc9e0882d2ef6ba54288ee
Tatiana Cieślar <tatianacieslar23@gmail.com>
2021-01-25 20:56:13 +0100

    Working on starting dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dialogueBox;
+    public bool dialogueActive;
+    GameObject toolbar;
+    public string[] sentences;
+    private int index;
+    public Text textDisplay;
+    public float typingSpeed;
+
+    public GameObject pressToContinue;
+
+    private void Awake()
+    {
+        toolbar = GameObject.FindWithTag("toolbar");
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toolbar.SetActive(false);
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            GoToNextSentence();
+        }
+        
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    public void GoToNextSentence()
+    {
+        //pressToContinue.SetActive(false);
+
+        if (index < sentences.Length - 1)
+        {
+            textDisplay.text = "";
+            index++;
+            StartCoroutine(Type());
+            
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                dialogueBox.SetActive(false);
+                toolbar.SetActive(true);
+            }
+            
+        }
+    }
+}
