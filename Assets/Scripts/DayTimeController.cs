6991d21d52d9796784c9c82c60fa2a5035dc0db9
Wojciech Kozieł <44378819+fern00@users.noreply.github.com>
2021-01-25 12:04:56 +0100

    temperature update

--- Assets/Scripts/DayTimeController.cs
+++ Assets/Scripts/DayTimeController.cs
@@ -13,12 +13,15 @@ public class DayTimeController : MonoBehaviour
     [SerializeField] float LightTransition = 0.0001f;
     public int hungerUpdaterCounter;
     public int healthUpdaterCounter;
+    public int temperatureUpdateCounter;
 
     private void Start()
     {
         time = 25200f;
         hungerUpdaterCounter = 0;
         healthUpdaterCounter = 0;
+        temperatureUpdateCounter = 0;
+        TemperatureController.currentTemperature = 100;
     }
     private float getHours
     {
@@ -33,20 +36,21 @@ public class DayTimeController : MonoBehaviour
 
     void Update()
     {
-        //próba wskaźnika głodu
+        //licznik wskaźnika głodu i temperatury
         hungerUpdaterCounter += 1;
+        temperatureUpdateCounter += 1;
         //tutaj dostosowac jak szybko maleje wskaznik najedzenia
-        if (hungerUpdaterCounter == 600)
+        if (hungerUpdaterCounter == 500)
         {
             HungerController.currentHunger -= 1;
             hungerUpdaterCounter = 0;
         }
-        //gdy wskaźnik najedzenia jest niższy niż 15, zaczyna ubywać zdrowia:
-        if(HungerController.currentHunger < 30)
+        //gdy wskaźnik najedzenia lub temperatury jest niższy niż 10, zaczyna ubywać zdrowia:
+        if(HungerController.currentHunger < 10 || TemperatureController.currentTemperature < 10)
         {
             healthUpdaterCounter += 1;
             //tutaj dostosowac jak szybko maleje wskaznik zdrowia
-            if (healthUpdaterCounter == 300)
+            if (healthUpdaterCounter == 100)
             {
                 HealthController.currentHealth -= 1;
                 healthUpdaterCounter = 0;
@@ -54,34 +58,55 @@ public class DayTimeController : MonoBehaviour
         }
         
 
+        //Kontrola czasu i wyświetlanie
         time += Time.deltaTime * TimeScale;
         int hours = (int)getHours;
         TimeDisplay.text = hours.ToString("00") + ":00";
         Light2D light = transform.GetComponent<Light2D>();
 
-        //Day light
+        //Światło dzienne od 4 do 20
         if (time > 25200f && time < 72000f)
         {
             light.intensity = 1f;
-            //proba wskaznika temperatury
-            TemperatureController.currentTemperature = 80;
+            
         }
-        //Lights down 20 - 4
+
+
+        //Rozjaśnia się w godzinach 20 - 4
         if ((time > 72000f && time < 86400f) || ((time > 0f && time < 14400f)))
+        {
             if (light.intensity > 0.3f)
             {
                 light.intensity -= LightTransition;
-                //proba wskaznika temperatury
-                TemperatureController.currentTemperature = 40;
-
             }
+            if (temperatureUpdateCounter > 100)
+            {
+                TemperatureController.currentTemperature -= 1;
+                temperatureUpdateCounter = 0;
+            }
+        }
+        
         //Lights up 4 - 7
         if (time > 14400f && time < 25200f)
-             if (light.intensity < 1f)
-                  light.intensity += LightTransition;
-
+        {
+            if (light.intensity < 1f)
+                light.intensity += LightTransition;
+            if (temperatureUpdateCounter > 100)
+            {
+                TemperatureController.currentTemperature -= 1;
+                temperatureUpdateCounter = 0;
+            }
+        }
+             
+        //Zmiana dnia na nowy
         if (time > SecondsInDay)
             time = 0;
+
+        //Jesli zdrowie spranie do 0 zmiana na scene game over
+        if(HealthController.currentHealth == 0)
+        {
+            //Zmiana sceny
+        }
     }
 
 }
