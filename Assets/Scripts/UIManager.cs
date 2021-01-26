ad8720bf524f35c505f3260131ad141299907d8b
PiotrHadam <drpiotr1997@gmail.com>
2021-01-26 16:43:52 +0100

    Pause added

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UIManager : MonoBehaviour
+{
+	GameObject[] pauseObjects;
+
+	void Start()
+	{
+		Time.timeScale = 1;
+		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused();
+	}
+
+	void Update()
+	{
+
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+			}
+			else if (Time.timeScale == 0)
+			{
+				Debug.Log("high");
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
+	}
+
+
+	public void Reload()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void pauseControl()
+	{
+		if (Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			showPaused();
+		}
+		else if (Time.timeScale == 0)
+		{
+			Time.timeScale = 1;
+			hidePaused();
+		}
+	}
+
+	public void showPaused()
+	{
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(true);
+		}
+	}
+
+	public void hidePaused()
+	{
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(false);
+		}
+	}
+
+	public void LoadLevel(string level)
+	{
+		Application.LoadLevel(level);
+	}
+
+	public void QuitGame()
+    {
+		Application.Quit();
+    }
+
+	public void LoadMenu()
+    {
+		Application.LoadLevel(0);
+    }
+}
